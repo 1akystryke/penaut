ca11481c83356b9e2c819a37b8ee64b9dcceab63
1akystryke <spiffyriver@gmail.com>
2026-05-20 14:15:24 +0300

    fix: model fix

--- internal/repository/postgres/channel.go
+++ internal/repository/postgres/channel.go
@@ -11,14 +11,14 @@ import (
 func (s *Store) CreateChannel(ctx context.Context, channelType string, channelName string) (model.Channel, error) {
 	var ch model.Channel
 	err := s.db.QueryRow(ctx,
-		`INSERT INTO channels (type,name) VALUES ($1,$2) RETURNING id, type,name,picture_path`,
+		`INSERT INTO channels (type,name) VALUES ($1,$2) RETURNING id, type,name, coalesce(picture_path, '') as picture_path`,
 		channelType, channelName,
-	).Scan(&ch.ID, &ch.Type, &ch.Name)
+	).Scan(&ch.ID, &ch.Type, &ch.Name, &ch.PicturePath)
 	return ch, err
 }
 
 func (s *Store) ListChannels(ctx context.Context, userId string) ([]model.Channel, error) {
-	rows, err := s.db.Query(ctx, `SELECT c.id, max(c.type) as type,max(c.name) as name , max(c.picture_path) as picture_path
+	rows, err := s.db.Query(ctx, `SELECT c.id, max(c.type) as type,max(c.name) as name , coalesce(max(c.picture_path), '') as picture_path
 				FROM channels as c
 				left join memberships m
 					on m.channel_id = c.id
--- internal/repository/postgres/user.go
+++ internal/repository/postgres/user.go
@@ -11,14 +11,14 @@ import (
 func (s *Store) CreateUser(ctx context.Context, name string, email string, passwordHash string) (model.User, error) {
 	var u model.User
 	err := s.db.QueryRow(ctx,
-		`INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id, name, email, password_hash,picture_path`,
+		`INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id, name, email, password_hash,coalesce(picture_path, '') as picture_path`,
 		name, email, passwordHash,
-	).Scan(&u.ID, &u.Name, &u.Email, &u.PasswordHash)
+	).Scan(&u.ID, &u.Name, &u.Email, &u.PasswordHash, &u.PicturePath)
 	return u, err
 }
 
 func (s *Store) ListUsers(ctx context.Context) ([]model.User, error) {
-	rows, err := s.db.Query(ctx, `SELECT id, name, email,'penis' as password_hash,picture_path FROM users ORDER BY created_at DESC`)
+	rows, err := s.db.Query(ctx, `SELECT id, name, email,'penis' as password_hash,coalesce(picture_path, '') as picture_path FROM users ORDER BY created_at DESC`)
 	if err != nil {
 		return nil, err
 	}
@@ -30,7 +30,7 @@ func (s *Store) ListUsers(ctx context.Context) ([]model.User, error) {
 func (s *Store) CheckPassword(ctx context.Context, email string, passwordHash string) (*model.User, error) {
 	//rows, err := s.db.Query(ctx, `SELECT id, name, email FROM users ORDER BY created_at DESC`)
 	rows, err := s.db.Query(ctx,
-		`Select id, name, email,password_hash,picture_path FROM users as u
+		`Select id, name, email,password_hash,coalesce(picture_path, '') as picture_path FROM users as u
 			where email = $1 and password_hash = $2 
 			LIMIT 1`,
 		email, passwordHash,
@@ -49,7 +49,7 @@ func (s *Store) GetUserbyToken(ctx context.Context, token string) (*model.User, 
 	//rows, err := s.db.Query(ctx, `SELECT id, name, email FROM users ORDER BY created_at DESC`)
 
 	rows, err := s.db.Query(ctx,
-		`select u.id, u.name, u.email,'penis' as password_hash,u.picture_path
+		`select u.id, u.name, u.email,'penis' as password_hash,coalesce(u.picture_path, '') as   picture_path
 		FROM users as u left join tokens as t
 		on t.user_id = u.id
 		where t."token" = $1
@@ -70,7 +70,7 @@ func (s *Store) GetUserbyID(ctx context.Context, userID string) (*model.User, er
 	//rows, err := s.db.Query(ctx, `SELECT id, name, email FROM users ORDER BY created_at DESC`)
 
 	rows, err := s.db.Query(ctx,
-		`select u.id, u.name, u.email,'penis' as password_hash,picture_path
+		`select u.id, u.name, u.email,'penis' as password_hash,coalesce(picture_path, '') as  picture_path
 		FROM users as u
 		where u."id" = $1
 		limit 1
